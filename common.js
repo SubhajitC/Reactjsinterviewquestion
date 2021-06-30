cad972e32aa8767043e070929b4e4768b4ea163a
Subhajit Chakraborty <41908790+SubhajitC@users.noreply.github.com>
2021-06-30 03:45:27 -0700

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,8 @@
-let k = [1,3,4,2,5,8,6,7,9];
-let j = [9,1,4,3,2,8,7,9,0,2,];  
-const common={k,j};          
+function getArraysIntersection(a1,a2){
+    return  a1.filter(function(n) { return a2.indexOf(n) !== -1;});
+}
+const k = [1,3,4,2,5,8,6,7,9];
+const j = [9,1,4,3,2,8,7,9,0,2,];     
+const common=getArraysIntersection(k,j);  
   
- console.log(common);
+console.log(common);
